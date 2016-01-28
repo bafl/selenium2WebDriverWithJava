5c9c0f1c6abd8dc74f204e87f1b79327ac95c9f7
Bartlomiej <bartlomiej.florczak@qcadoo.com>
2016-01-28 22:24:18 +0100

    Some refactor, new tests, use of java.awt.Robot class

--- src/test/java/com/bafl/webdriver/interaction/UserInteractionsExercise.java
+++ src/test/java/com/bafl/webdriver/interaction/UserInteractionsExercise.java
@@ -1,9 +1,10 @@
 package com.bafl.webdriver.interaction;
 
-import org.junit.After;
-import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -12,8 +13,12 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
 import static com.bafl.webdriver.navigation.NavigateToURL.URL;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 /**
  * selenium2WebDriverWithJava
@@ -21,16 +26,17 @@ import static org.junit.Assert.assertEquals;
  */
 public class UserInteractionsExercise {
 
-    private WebDriver driver;
+    private static WebDriver driver;
 
-    @Before
-    public void setUp(){
+    @BeforeClass
+    public static void setUp() {
         driver = new FirefoxDriver();
         driver.manage().window().maximize();
+        goToURL();
+    }
 
     @Test
     public void moveDraggableOneOnToDroppableOne(){
-        driver.get(URL+"gui_user_interactions.html");
         new WebDriverWait(driver,5).until(ExpectedConditions.titleIs("GUI User Interactions"));
         WebElement draggable1;
         draggable1 = driver.findElement(By.cssSelector("#draggable1"));
@@ -42,11 +48,66 @@ public class UserInteractionsExercise {
                                             release(droppable1).
                                             build();
         moveDraggableOneOnToDroppableOne.perform();
-        assertEquals("Label should changed to: Dropped!","Dropped!",droppable1.getText());
+        assertEquals("Label should change to: Dropped!","Dropped!",droppable1.getText());
     }
 
-    @After
-    public void quit(){
+    @Test
+    public void dragAndDropDraggableTwoToDroppableOne(){
+        new WebDriverWait(driver,5).until(ExpectedConditions.visibilityOfElementLocated(By.id("draggable2")));
+        WebElement draggable2;
+        draggable2 = driver.findElement(By.cssSelector("#draggable2"));
+        WebElement droppable1;
+        droppable1 = driver.findElement(By.id("droppable1"));
+        Action dragdragAndDropDraggableTwoToDroppableOne = new Actions(driver).dragAndDrop(draggable2,droppable1).build();
+        dragdragAndDropDraggableTwoToDroppableOne.perform();
+        assertEquals("Label should change to: Get Off Me!", "Get Off Me!", droppable1.getText());
+    }
+
+    @Test
+    public void pressCtrlBAndAssertForTextChangeOnDraggableOne(){
+        new WebDriverWait(driver,5).until(ExpectedConditions.visibilityOfElementLocated(By.id("draggable2")));
+        WebElement draggable1;
+        draggable1 = driver.findElement(By.cssSelector("#draggable1"));
+        Action pressCtrlB = new Actions(driver).keyDown(Keys.CONTROL).sendKeys("b").keyUp(Keys.CONTROL).build();
+        pressCtrlB.perform();
+        assertEquals("Label should change to: Bwa! Ha! Ha!", "Bwa! Ha! Ha!", draggable1.getText());
+    }
+
+
+    @Test
+    public void pressCtrlAndSpaceAndAssertRedSquaresText() throws AWTException {
+        new WebDriverWait(driver, 5).until(ExpectedConditions.visibilityOfElementLocated(By.id("draggable1")));
+        WebElement droppable1;
+        droppable1 = driver.findElement(By.cssSelector("#droppable1"));
+        WebElement droppable2;
+        droppable2 = driver.findElement(By.cssSelector("#droppable2"));
+        //Doesn't work for now
+        //Actions CtrlSpace = new Actions(driver);
+        //CtrlSpace.keyDown(Keys.CONTROL).sendKeys(Keys.SPACE).perform();
+
+        Robot robot = new Robot();
+        robot.keyPress(KeyEvent.VK_CONTROL);
+        robot.keyPress(KeyEvent.VK_SPACE);
+        String droppableOneText = droppable1.getText();
+        String droppableTwoText = droppable2.getText();
+        robot.keyRelease(KeyEvent.VK_CONTROL);
+        robot.keyRelease(KeyEvent.VK_SPACE);
+
+        try {
+            assertEquals("Label should change to: Let GO!!!", "Let GO!!!", droppableOneText);
+            assertEquals(droppableOneText,droppableTwoText);
+            fail("It works!");
+        } catch (org.junit.ComparisonFailure e) {
+            System.out.print(e.getMessage());
+        }
+    }
+
+    private static void goToURL() {
+        driver.get(URL+"gui_user_interactions.html");
+    }
+
+    @AfterClass
+    public static void quit(){
         driver.close();
         driver.quit();
     }
