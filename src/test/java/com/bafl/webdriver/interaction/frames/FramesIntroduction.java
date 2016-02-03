0855a5fab3b3cf162cbefd4f58e0564e3884a936
Bartlomiej <bartlomiej.florczak@qcadoo.com>
2016-02-03 21:57:58 +0100

    Test completed + some refactor

--- src/test/java/com/bafl/webdriver/interaction/frames/FramesIntroduction.java
+++ src/test/java/com/bafl/webdriver/interaction/frames/FramesIntroduction.java
@@ -8,12 +8,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.pagefactory.ByChained;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static com.bafl.webdriver.navigation.NavigateToURL.URL;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
 
 /**
  * selenium2WebDriverWithJava
@@ -21,37 +22,52 @@ import static org.junit.Assert.assertTrue;
  */
 public class FramesIntroduction {
     private static WebDriver driver;
+    private static WebDriverWait wait;
 
     @BeforeClass
     public static void setUp(){
         driver = new FirefoxDriver();
         driver.navigate().to(URL + "frames");
+        wait = new WebDriverWait(driver,10000);
     }
 
     @Test
     public void assertPresenceOfElementAfterReturnFromOtherFrame(){
-        assertEquals(driver.getTitle(),"Frameset Example Title (Example 6)");
+        assertEquals("Frameset Example Title (Example 6)",driver.getTitle());
         driver.switchTo().frame(2);
         WebElement greenPageLink;
         greenPageLink = driver.findElement(By.cssSelector("a[href=\"green.html\"]"));
         greenPageLink.click();
-        new WebDriverWait(driver,10000).until(ExpectedConditions.visibilityOfElementLocated(By.id("green")));
+        wait.until(visibilityOfElementLocated(By.id("green")));
         assertEquals(driver.findElement(By.tagName("h1")).getText(),"Green Page");
         driver.findElement(new ByChained(By.tagName("p"),By.tagName("a"))).click();
         assertTrue(driver.findElement(By.cssSelector("html>body>h1")).isDisplayed());
-        assertEquals(driver.findElement(By.cssSelector("html>body>h1")).getText(),"Content");
+        assertEquals("Content",driver.findElement(By.cssSelector("html>body>h1")).getText());
     }
 
     @Test
     public void iFrameExercise(){
+        WebElement iFramesExampleLink;
+        WebElement exampleFiveLink;
+        WebElement loadMainFramesPageLink;
+
         assertEquals(driver.getTitle(),"Frameset Example Title (Example 6)");
         driver.switchTo().frame("menu");
-        WebElement iFramesExampleLink;
         iFramesExampleLink = driver.findElement(By.cssSelector("body > a:last-of-type"));
         iFramesExampleLink.click();
-        new WebDriverWait(driver,10000).until(ExpectedConditions.titleIs("HTML Frames Example - iFrame Contents"));
+        waitForTitle("HTML Frames Example - iFrame Contents");
         driver.switchTo().frame(0);
+        exampleFiveLink = driver.findElement(By.linkText("Example 5"));
+        exampleFiveLink.click();
+        waitForTitle("Frameset Example Title (Example 5)");
+        driver.switchTo().frame(2);
+        loadMainFramesPageLink = driver.findElement(By.xpath("//ul[1]/li/a"));
+        loadMainFramesPageLink.click();
+        assertEquals("Expected title: Frameset Example Title (Example 6)","Frameset Example Title (Example 6)",driver.getTitle());
+    }
 
+    private void waitForTitle(String title) {
+        wait.until(titleIs(title));
     }
 
     @AfterClass
